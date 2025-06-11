953a045221d1fc4ab6854c514c7d92453797cd7b
sepideh <sepideh0771@gmail.com>
2025-06-11 16:10:54 -0700

    layout

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,12 +1,40 @@
 import React from "react";
-import Layout from "../components/Layout/Layout";
+import { MenuList } from "../data/data";
+import Layout from "./../components/Layout/Layout";
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
 
-function Menu() {
+const Menu = () => {
   return (
     <Layout>
-      <h1>Menu Page</h1>
+      <Box sx={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
+        {MenuList.map((menu) => (
+          <Card sx={{ maxWidth: "390px", display: "flex", m: 2 }}>
+            <CardActionArea>
+              <CardMedia
+                sx={{ minHeight: "400px" }}
+                component={"img"}
+                src={menu.image}
+                alt={menu.name}
+              />
+              <CardContent>
+                <Typography variant="h5" gutterBottom component={"div"}>
+                  {menu.name}
+                </Typography>
+                <Typography variant="body2">{menu.description}</Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        ))}
+      </Box>
     </Layout>
   );
-}
+};
 
 export default Menu;
