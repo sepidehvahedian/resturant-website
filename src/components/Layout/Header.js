6cd3ba8b9ad6ae99a3be8d7a8d8331cade3ea49b
sepideh <sepideh0771@gmail.com>
2025-06-11 12:02:34 -0700

    add font roboto

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import {
   AppBar,
   Box,
@@ -8,20 +9,37 @@ import {
   IconButton,
 } from "@mui/material";
 import FastfoodIcon from "@mui/icons-material/Fastfood";
-
+import '../../styles/HeaderStyles.css'
 function Header() {
   return (
     <>
-      <Box >
-        <AppBar component={'nav'} sx={{bgcolor:'black'}}>
+      <Box>
+        <AppBar component={"nav"} sx={{ bgcolor: "black" }}>
           <Toolbar>
-       
-           
-          
-            <Typography color={'goldenrod'} variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              <FastfoodIcon />   My Resturent
+            <Typography
+              color={"goldenrod"}
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1 }}
+            >
+              <FastfoodIcon /> My Resturent
             </Typography>
-       
+            <Box sx={{ display: { xs: "none", sm: "block" } }}>
+              <ul className="navigation-menu">
+                <li>
+                  <Link to={"/"}>Home</Link>
+                </li>
+                <li>
+                  <Link to={"/Menu"}>Menu</Link>
+                </li>
+                <li>
+                  <Link to={"/About"}>About</Link>
+                </li>
+                <li>
+                  <Link to={"/Contact"}>Contact</Link>
+                </li>
+              </ul>
+            </Box>
           </Toolbar>
         </AppBar>
       </Box>
